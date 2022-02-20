daf45cd4ccaf41c523ed9585f5d810f594e7b8d9
ToKiNoBug <tokinobug@163.com>
2022-02-20 22:42:06 +0800

    First fractal

---
+++ Draw.m
@@ -0,0 +1,18 @@
+Rows=100;
+Cols=100;
+load('BegStatue00.mat')
+
+Ms1=1;
+Ms2=logspace(log10(0.5),log10(2),Rows)*MassVec(2);
+Ms3=logspace(log10(0.5),log10(2),Cols)*MassVec(3);
+
+lastTimeMat=zeros(Rows,Cols);
+noCollideMat=false(Rows,Cols);
+
+for r=1:Rows
+   for c=1:Cols
+      [noCollideMat(r,c),lastTimeMat(r,c)]=runThreeBody([Ms1,Ms2(r),Ms3(c)],BegPos,BegVelocity,tSpan); 
+   end
+   disp(num2str(r))
+end
+
